803cde9897aa24e0a5e8cc6c956169e374ff96da
kanishkasd <kanishkasandaru2048@gmail.com>
2025-03-22 21:43:53 +0530

    day 4 of coding

--- Library/index.js
+++ Library/index.js
@@ -1,7 +1,3 @@
-const uuid = crypto.randomUUID();
-const addBookButton = document.getElementById("newBook");
-const newBookForm = document.querySelector(".new_book");
-
 const myLibrary = [
   {
     id: crypto.randomUUID(),
@@ -17,12 +13,66 @@ const myLibrary = [
   },
 ];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-function addBookToLibrary() {
-  addBookButton.addEventListener("click", () => {
-    newBookForm.classList.toggle("hidden");
+Book.prototype.toggleReadStatus = function () {
+  this.read = !this.read;
+  displayBooks();
+};
+
+function addBookToLibrary(author, title, pages, read) {
+  const newBook = new Book(author, title, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
+}
+
+function displayBooks() {
+  const libraryDiv = document.getElementById("library");
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+
+    bookCard.innerHTML = `
+    <h3>${book.title}</h3>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Read: ${book.read ? "✅ Yes" : "❌ No"}</p>
+    <button onclick="toggleRead('${book.id}')">Toggle Read<button> 
+ <button onclick="removeBook('${book.id}')">Remove</button>`;
+    libraryDiv.appendChild(bookCard);
   });
 }
 
-addBookToLibrary();
+function toggleRead(bookId) {
+  const book = myLibrary.find((book) => book.id === bookId);
+  if (book) {
+    book.toggleReadStatus();
+  }
+}
+
+//show hide form
+document.getElementById("newBook").addEventListener("click", () => {
+  document.querySelector(".new_book").classList.toggle("hidden");
+});
+
+// handle form submit
+document
+  .querySelector(".new_book")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const author = document.getElementById("author").value;
+    const title = document.getElementById("title").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(author, title, pages, read);
+    this.reset();
+  });
